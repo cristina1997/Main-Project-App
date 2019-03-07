8c55fcc895998546f70aa5994915fdd884737d1d
unknown <cristinaflorinaeu@gmail.com>
2019-03-07 18:03:51 +0000

    Added Image Adapter

--- CameraDetection/app/src/main/java/sw/gmit/ie/crist/cameradetection/Activities/GalleryFragment.java
+++ CameraDetection/app/src/main/java/sw/gmit/ie/crist/cameradetection/Activities/GalleryFragment.java
@@ -1,8 +1,8 @@
 package sw.gmit.ie.crist.cameradetection.Activities;
 
+
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.support.annotation.*;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,12 +10,13 @@ import android.view.ViewGroup;
 
 import sw.gmit.ie.crist.cameradetection.R;
 
-public class MessageFragment extends Fragment {
+public class GalleryFragment extends Fragment {
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_message, container, false);
+        return inflater.inflate(R.layout.fragment_gallery, container, false);
 
     }
 }
