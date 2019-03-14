decce604c3c19b9723fae94c0f83d5d98b005dbb
unknown <cristinaflorinaeu@gmail.com>
2019-03-14 03:06:02 +0000

    Taking and choosing pictures through action buttons - Take pictures to be fixed

--- CameraDetection/app/src/main/java/sw/gmit/ie/crist/cameradetection/Activities/Home.java
+++ CameraDetection/app/src/main/java/sw/gmit/ie/crist/cameradetection/Activities/Home.java
@@ -1,11 +1,12 @@
 package sw.gmit.ie.crist.cameradetection.Activities;
 
 import android.app.DownloadManager;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -15,42 +16,45 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.webkit.DownloadListener;
+import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.messaging.RemoteMessage;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
-import com.pusher.pushnotifications.PushNotificationReceivedListener;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 import com.pusher.pushnotifications.PushNotifications;
-import com.pusher.pushnotifications.auth.AuthData;
-import com.pusher.pushnotifications.auth.AuthDataGetter;
-import com.pusher.pushnotifications.auth.BeamsTokenProvider;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.regex.Pattern;
 
 import sw.gmit.ie.crist.cameradetection.R;
 
-public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, NameDialog.NameDialogListener {
     private static final int PICK_IMAGE_REQUEST = 1;
-    private boolean isSignedIn;
+    private static final int CAPTURE_IMAGE_REQUEST = 0;
+    private boolean isSignedIn, isChosen;
     private DrawerLayout drawer;
     private Toolbar toolbar;
     private NavigationView navigationView;
     private ActionBarDrawerToggle toggle;
     private FirebaseUser user;
+    private StorageTask uploadTask;
+    private Uri imgURI;
 
+    public boolean getIsChosen() {
+        return isChosen;
+    }
+    public void setIsChosen(boolean isChosen) {
+        this.isChosen = isChosen;
+    }
     public boolean getSignedIn() {
         return isSignedIn;
     }
@@ -126,13 +130,17 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new GalleryFragment()).commit();
                 break;
             case R.id.nav_call_911:
-                showMessage("Calling 911");
+
                 break;
             case R.id.nav_add_photo:
-                choosePicture();
+                setIsChosen(true);
+                openDialog();
+//                choosePicture();
                 break;
             case R.id.nav_take_photo:
-                takePicture();
+                setIsChosen(false);
+                openDialog();
+//                takePicture();
                 break;
             case R.id.nav_download_video:
                 downloadVideo();
@@ -181,17 +189,104 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
 
     }
 
-    private void choosePicture() {
+    private void openDialog() {
+        NameDialog nameDialog = new NameDialog();
+        nameDialog.show(getSupportFragmentManager(), "example dialog");
+    }
+
+    private void choosePicture(String name) {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(intent, PICK_IMAGE_REQUEST);
+        userFirebaseStorage(name);
     }
 
-    private void takePicture() {
+    private void takePicture(String name) {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, 0);
+        startActivityForResult(intent, CAPTURE_IMAGE_REQUEST);
 
+        userFirebaseStorage(name);
+    }
+
+    private void userFirebaseStorage(String name){
+        String userDisplayName = user.getDisplayName();
+        String personName = name;
+        StorageReference imageStorageRef = FirebaseStorage.getInstance().getReference("images/" +userDisplayName+ "/" + personName);
+        DatabaseReference imageDatabaseRef = FirebaseDatabase.getInstance().getReference("images/" +userDisplayName);
+        uploadImage (imageStorageRef, imageDatabaseRef, name);
+    }
+
+    private void uploadImage(StorageReference imageStorageRef, DatabaseReference imageDatabaseRef, String name) {
+
+        if (uploadTask != null && uploadTask.isInProgress()) {}
+        else { uploadFile(imageStorageRef, imageDatabaseRef, name); }
+
+    }
+
+    private String getFileExtension(Uri uri) {
+        // get extension from all files
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
+
+    private void uploadFile(StorageReference imageStorageRef, final DatabaseReference imageDatabaseRef, final String name) {
+
+        if (imgURI != null) {
+            final StorageReference fileReference = imageStorageRef.child(System.currentTimeMillis() + "." + getFileExtension(imgURI));
+
+            uploadTask = fileReference.putFile(imgURI)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            //  Successful upload
+                            // Code adapted from: https://stackoverflow.com/questions/50570893/after-upload-a-file-in-android-firebase-storage-how-get-the-file-download-url-g/50572357
+                            fileReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    Handler handler = new Handler();
+                                    handler.postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+
+
+                                        }
+                                    }, 500);
+
+                                    Upload upload = new Upload(name.trim(),
+                                            uri.toString());
+                                    showMessage("URI: " +uri.toString());
+                                    String uploadId = imageDatabaseRef.push().getKey();
+
+                                    imageDatabaseRef.child(uploadId).setValue(upload);
+                                }
+                            });
+                        };
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            //  Failed upload
+                            showMessage("Failed to upload image");
+
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot> () {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            //  Progressing upload - update progress bar with current progress
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                        }
+                    });
+
+        } else {
+            if (getIsChosen () == true){
+                showMessage("No file selected");
+            } else {
+                showMessage("No picture taken");
+            }
+        }
     }
 
     private void downloadVideo() {
@@ -204,7 +299,7 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
             @Override
             public void onSuccess(Uri uri) {
                 String url = uri.toString();
-                downloadFiles(Home.this, "Folders", ".avi", Environment.DIRECTORY_MUSIC + "", url);
+                downloadFiles(Home.this, "Folders", ".avi", Environment.DIRECTORY_DOWNLOADS, url);
             }
 
         }).addOnFailureListener(new OnFailureListener() {
@@ -226,7 +321,7 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         Uri uri = Uri.parse(url);
         DownloadManager.Request request = new DownloadManager.Request(uri);
 
-        request.setDestinationInExternalFilesDir(context, Environment.DIRECTORY_DOWNLOADS, fileName + fileExtension);
+        request.setDestinationInExternalFilesDir(context, destinationDirectory, fileName + fileExtension);
         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
 
         downloadManager.enqueue(request);
@@ -241,6 +336,15 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
 //        pf.imgView.setImageBitmap(bitmap);
 //    }
 
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if ((requestCode == PICK_IMAGE_REQUEST )/*|| requestCode == CAPTURE_IMAGE_REQUEST)*/ && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            imgURI = data.getData();
+        }
+    }
+
     @Override
     public void onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)){
@@ -254,5 +358,12 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
     }
 
-
+    @Override
+    public void applyTexts(String personName) {
+        if (getIsChosen () == true){
+            choosePicture (personName);
+        } else {
+            takePicture (personName);
+        }
+    }
 }
